285cb1c6357688aaefb940cf24a10613d6f0dc45
Ardit Shala <31743528+arditshala@users.noreply.github.com>
2019-03-09 00:58:30 +0100

    Add files via upload

---
+++ Linked_List/Problem_1_a_Linked_List.cpp
@@ -0,0 +1,124 @@
+/*
+    Ardit Shala ADS - Spring 2018
+    a.shala@jacobs-university.de
+    30001420 / April 04 2018
+    Problem 1: (a)
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class StackUnderFlowException
+{
+public:
+    StackUnderFlowException()
+    {
+        cout << "Stack underflow" << endl;
+    }
+};
+
+template <typename T>
+class stack{
+public:
+    stack();
+    stack(int new_size);
+    void push(T);
+    void pop(T &);
+    bool isEmpty(void);
+
+private:
+    struct node{
+    node(){  next = NULL; }
+    T data;
+    node* next;
+ }*top;
+
+};
+
+template <typename T>
+stack<T>::stack(int new_size)
+{
+    top = new_size;
+}
+
+
+template <typename T>
+stack<T>::stack()
+{
+    top = NULL;
+}
+
+
+
+template <typename T>
+void stack<T>::push(T x)
+{
+    node* newNode = new node;
+    newNode->data = x;
+
+    if (top != NULL)
+    {
+        newNode->next = top;
+        top = newNode;
+    }
+    else
+    {
+        newNode->next = NULL;
+        top = newNode;
+    }
+}
+
+
+template <typename T>
+void stack<T>::pop(T &x){
+    if (isEmpty())
+    {
+        throw new StackUnderFlowException();
+    }
+    else
+    {
+        x = top->data;
+        node* temp = top;
+        top = top->next;
+        delete temp;
+        temp = NULL;
+    }
+}
+
+
+template <typename T>
+bool stack<T>::isEmpty(void)
+{
+    return top == NULL;
+}
+
+
+int main(){
+
+    stack<int> s;
+
+    try
+    {
+        if(s.isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+        }
+
+        s.push(100);
+        s.push(200);
+
+        int x;
+
+        for (int i = 1; i<=5; i++)
+        {
+            s.pop(x);
+            cout << x << endl;
+        }
+    }
+
+    catch(...)
+    {
+        cout << "Some exception occured" << endl;
+    }
+}
